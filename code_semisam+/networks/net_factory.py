29508e5deac351a84649a7afda0fb3462702fac8
Thong Luc <email_github_cua_ban@gmail.com>
2026-04-04 15:33:25 +0700

    update net_factory

--- code_semisam+/networks/net_factory.py
+++ code_semisam+/networks/net_factory.py
@@ -68,31 +68,32 @@ parser.add_argument('--consistency', type=float,
                     default=0.1, help='consistency')
 parser.add_argument('--consistency_rampup', type=float,
                     default=200.0, help='consistency_rampup')
-args = parser.parse_args()
-config = get_config(args)
-
+                    
 
 def net_factory(net_type="unet", in_chns=1, class_num=3):
     if net_type == "unet":
         net = UNet(in_chns=in_chns, class_num=class_num).cuda()
-    elif net_type == "enet":
-        net = ENet(in_channels=in_chns, num_classes=class_num).cuda()
-    elif net_type == "unet_ds":
-        net = UNet_DS(in_chns=in_chns, class_num=class_num).cuda()
-    elif net_type == "unet_cct":
-        net = UNet_CCT(in_chns=in_chns, class_num=class_num).cuda()
-    elif net_type == "unet_urpc":
-        net = UNet_URPC(in_chns=in_chns, class_num=class_num).cuda()
-    elif net_type == "efficient_unet":
-        net = Effi_UNet('efficientnet-b3', encoder_weights='imagenet',
-                        in_channels=in_chns, classes=class_num).cuda()
-    elif net_type == "ViT_Seg":
-        net = ViT_seg(config, img_size=args.patch_size,
-                      num_classes=args.num_classes).cuda()
-    elif net_type == "pnet":
-        net = PNet2D(in_chns, class_num, 64, [1, 2, 4, 8, 16]).cuda()
-    elif net_type == "nnUNet":
-        net = initialize_network(num_classes=class_num).cuda()
+    # elif net_type == "enet":
+    #     net = ENet(in_channels=in_chns, num_classes=class_num).cuda()
+    # elif net_type == "unet_ds":
+    #     net = UNet_DS(in_chns=in_chns, class_num=class_num).cuda()
+    # elif net_type == "unet_cct":
+    #     net = UNet_CCT(in_chns=in_chns, class_num=class_num).cuda()
+    # elif net_type == "unet_urpc":
+    #     net = UNet_URPC(in_chns=in_chns, class_num=class_num).cuda()
+    # elif net_type == "efficient_unet":
+    #     net = Effi_UNet('efficientnet-b3', encoder_weights='imagenet',
+    #                     in_channels=in_chns, classes=class_num).cuda()
+    # elif net_type == "ViT_Seg":
+    #     net = ViT_seg(config, img_size=args.patch_size,
+    #                   num_classes=args.num_classes).cuda()
+    # elif net_type == "pnet":
+    #     net = PNet2D(in_chns, class_num, 64, [1, 2, 4, 8, 16]).cuda()
+    # elif net_type == "nnUNet":
+    #     net = initialize_network(num_classes=class_num).cuda()
     else:
+        # Nếu truyền nhầm tên khác, vẫn trả về UNet để không bị sập code
+        print(f"Warning: Model {net_type} không xác định, mặc định sử dụng UNet 2D.")
+        net = UNet(in_chns=in_chns, class_num=class_num).cuda()
         net = None
     return net
